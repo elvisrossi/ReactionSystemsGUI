284f13640999abf73dea8ae795fe85cf0c825503
elvis <elvilio98@gmail.com>
2025-10-29 17:16:14 +0100

    ReadMe

--- reaction_systems_gui/build_web.sh
+++ reaction_systems_gui/build_web.sh
@@ -28,14 +28,9 @@ while test $# -gt 0; do
   esac
 done
 
-# ./setup_web.sh # <- call this first!
-
 CRATE_NAME="reaction_systems_gui"
-CRATE_NAME_SNAKE_CASE="${CRATE_NAME//-/_}" # for those who name crates with-kebab-case
+CRATE_NAME_SNAKE_CASE="${CRATE_NAME//-/_}"
 
-# This is required to enable the web_sys clipboard API which egui_web uses
-# https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.Clipboard.html
-# https://rustwasm.github.io/docs/wasm-bindgen/web-sys/unstable-apis.html
 export RUSTFLAGS='--cfg=web_sys_unstable_apis --cfg getrandom_backend="wasm_js"'
 
 # Clear output from old stuff:
